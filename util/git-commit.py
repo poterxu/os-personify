d3fe46b897e5dbb27fb4cd25a03d6ddbb00aacfc
Xu Xiaopeng <xiaopengx.xu@intel.com>
2017-03-16 12:15:08 +0800

    wrapper git commit cmd
    Signed-off-by: Xu Xiaopeng <xiaopengx.xu@intel.com>

--- util/git-commit.py
+++ util/git-commit.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
+print sys.argv[1:]
+commit_arg=''
+for i in range(1, len(sys.argv)):
+    commit_arg+=' '
+    commit_arg+=sys.argv[i]
 
-commit_arg=sys.argv[1]
-git_commit_cmd='git commit '+commit_arg
+
+
+git_commit_cmd='git commit'+commit_arg
 os.system(git_commit_cmd)
