f452483e4fecaa81c6276633678602da0f96d3c7
Xu Xiaopeng <xiaopengx.xu@intel.com>
2017-03-16 17:46:34 +0800

    install nessesary tools

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
-PYTHON_PROGRAM="python"
-command -v $PYTHON_PROGRAM > /dev/null 2>&1 || { echo >&2 "python not installed, start install..."; }
+function program_install() {
+	PROGRAM_NAME=$1
+	command -v $PROGRAM_NAME > /dev/null 2>&1 || { echo >&2 "$PROGRAM_NAME not installed, start install..."; sudo apt-get install $PROGRAM_NAME; }
+}
+program_install rar;
+program_install python;
+program_install git;
+program_install cscope;
+program_install vim;
